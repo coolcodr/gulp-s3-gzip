d7e68e5c97786147c6d19ed40bc28782ab5ad4b7
Matthew Lai <matthew.lai@nix-digital.com>
2015-10-08 06:40:52 -0700

    Use steam-to-queue to control concurrency

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var es = require('event-stream');
 var knox = require('knox');
 var gutil = require('gulp-util');
 var mime = require('mime');
+var streamToQueue = require('stream-to-queue');
 mime.default_type = 'text/plain';
 
 module.exports = function (aws, options) {
@@ -16,10 +17,10 @@ module.exports = function (aws, options) {
   var regexGzip = /\.([a-z]{2,})\.gz$/i;
   var regexGeneral = /\.([a-z]{2,})$/i;
 
-  return es.mapSync(function (file) {
+  return streamToQueue(function (file, cb) {
 
       // Verify this is a file
-      if (!file.isBuffer()) { return file; }
+      if (!file.isBuffer()) { return cb(null, file); }
 
       var uploadPath = file.path.replace(file.base, options.uploadPath || '');
       uploadPath = uploadPath.replace(new RegExp('\\\\', 'g'), '/');
@@ -29,12 +30,12 @@ module.exports = function (aws, options) {
               headers[key] = options.headers[key];
           }
       }
-      
+
       var hasGzipExtension = false;
       if (regexGzip.test(file.path)) {
           // Set proper encoding for gzipped files, remove .gz suffix
           headers['Content-Encoding'] = 'gzip';
-          
+
           if (options.removeGzipExtension) {
             uploadPath = uploadPath.substring(0, uploadPath.length - 3);
           } else {
@@ -42,9 +43,9 @@ module.exports = function (aws, options) {
           }
       } else if (options.gzippedOnly) {
         // Ignore non-gzipped files
-        return file;
+        return cb(null, file);
       }
-      
+
       // special case file extension handler for .gzipped files
       var sanitizedUploadPath = uploadPath;
       if (hasGzipExtension) {
@@ -57,19 +58,19 @@ module.exports = function (aws, options) {
         if (options.encoding) {
           headers['Content-Type'] += '; charset=' + options.encoding;
         }
-      } 
+      }
 
       headers['Content-Length'] = file.stat.size;
 
       client.putBuffer(file.contents, uploadPath, headers, function(err, res) {
         if (err || res.statusCode !== 200) {
           gutil.log(gutil.colors.red('[FAILED]', file.path + " -> " + uploadPath));
+          gutil.log(gutil.colors.red('[ERROR]', err));
         } else {
           gutil.log(gutil.colors.green('[SUCCESS]', file.path + " -> " + uploadPath));
           res.resume();
         }
+        return cb(null, file);
       });
-
-      return file;
-  });
+  }, 10);
 };
